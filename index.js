27851533f4c5b1b175070af45d5970da75e606d2
Manuel De Jesus <manuel.ernestodjs@gmail.com>
2023-05-13 09:18:35 -0400

    update the image in the podcast and remove srcset attribute

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const getPodcast = (podcasts) => {
     newPodcast.innerHTML = `
   
     <picture class="menu__content-image flex">
-      <img class="card-image" src="https://picsum.photos/id/96/150" alt="One of the images" srcset="https://picsum.photos/id/96/200">
+      <img class="card-image" src="./images/podcast-640x427.jpg" alt="Podcast">
     </picture>
 
     <h3 class="menu__content-title flex">${podcast.name}</h2>
